2effc8c16a4ed96004966c56cfa16ed7904859bc
priscillamc <priscillamc@users.noreply.github.com>
2017-05-17 12:38:22 -0400

    Initial commit

---
+++ ufclas-gcl-widget.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     UFCLAS Gator CareerLink Widget
+ * Plugin URI:      https://it.clas.ufl.edu/
+ * Description:     Widget and shortcode for embedding Symplicity Job Widgets in WordPress
+ * Author:          Priscilla Chapman (CLAS IT)
+ * Author URI:      https://it.clas.ufl.edu/
+ * Text Domain:     ufclas-gcl-widget
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Ufclas_Gcl_Widget
+ */
